a736dbb04cdd25abd426245b0f10eaa28638e9f0
Piimeentaa <dora13.isa@gmail.com>
2019-09-30 19:09:59 -0300

    Atualização exercicios sexta

--- resources/js/exercicio1.js
+++ resources/js/exercicio1.js
@@ -1,33 +1,17 @@
-function classificaAngulo(valor){
-  if( valor > 0 && valor < 90)
-  {
-    return ('Ângulo agudo');
-    
-  } else if( valor === 90 )
-  {
-    return ('Ângulo reto');
+function carrinhoDeCompras(codigoItem,quantidade){
+  if(codigoItem == 100){
+      console.log('Cachorro-Quente ' + ' R$ ' + quantidade * 1.70)
+  } else if(codigoItem == 101){
+      console.log('Bauru Simples '  + ' R$ ' + quantidade *  2.30 )
+  }else if(codigoItem == 102){
+    console.log('Bauru com ovo '  + ' R$ ' + quantidade *  2.60 )
+  }else if(codigoItem == 103){
+    console.log('Hamburguer '  + ' R$ ' + quantidade *  2.40 )
+  }else if(codigoItem == 104){
+    console.log('Cheeseburguer '  + ' R$ ' + quantidade *  2.50 )
+  }else if(codigoItem == 105){
+    console.log('Refrigerante '  + ' R$ ' + quantidade *  1.00 )
   }
-  else if( valor > 90 && valor < 180 )
-  {
-    return ('Ângulo obtuso');
-    
-  }
-  else if( valor === 0 || valor === 180 )
-  {
-    return ('Ângulo raso');
-  } 
-  else if( valor > 180 && valor < 360)
-  {
-    return ('Ângulo Côncavo');
-  } 
-  else if ( valor === 360)
-  {
-    return ('Ângulo completo');
-  } 
-  else 
-  { 
-    return ('Não sabe escrever, vagabundo');
-  } 
 }
 
-console.log(classificaAngulo(45));
+carrinhoDeCompras(100,2);
