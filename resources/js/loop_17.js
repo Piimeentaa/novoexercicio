e3d66214af44799044da62406a26cacbc1c5e373
Isa Romero <41489946+Piimeentaa@users.noreply.github.com>
2019-10-04 21:38:06 -0300

    Update loop_17.js

--- resources/js/loop_17.js
+++ resources/js/loop_17.js
@@ -8,3 +8,31 @@ for(let i = 0; i < 8; i++){
     }
     console.log(entrada);
 }
+//for (let i = 0; i <= 5; i++){
+ //   for(let y = 1; y = <= i; y++){
+ //       
+ //   }
+//  console.log(i);
+//}
+//for(let i = 0; i < 8; i++){
+ //   let entrada = " ";
+//
+//    for(h = 0; h < (8 - i); h++){
+//    entrada += "*";
+//    
+//    }
+//    console.log(entrada);
+//}
+function verificarPangrama(frase){
+    let letra ='abcdefghijlmnopqrstuvxz';
+    letra  = letra.split('');
+    frase = frase.toLowerCase();
+    
+    for(let i = 0; i < letra.length ; i++){
+        if(frase.indexOf(letra[i])< 0){
+            return false;
+        }   
+    }
+    return true;
+} 
+console.log(verificarPangrama('Blitz prende ex-vesgo com cheque fajuto.'));
