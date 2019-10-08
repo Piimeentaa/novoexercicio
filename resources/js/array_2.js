7b85ffd70232b909b2bed3496adbde2a04ae856e
Piimeentaa <dora13.isa@gmail.com>
2019-10-07 22:28:48 -0300

    novas alterações

--- resources/js/array_2.js
+++ resources/js/array_2.js
@@ -1,4 +1,4 @@
 function comprimentoMedioPalavras(palavra1,palavra2){
     return (palavra1.length + palavra2.length) / 2 
 }
-console.log(comprimentoMedioPalavras('banana','coco'));
+console.log(comprimentoMedioPalavras('banana','coco'))
